7357545279d7c6baf7e9c0309431a08292981519
beeglebug <updownleftright@gmail.com>
2015-03-15 10:33:15 +0000

    branching map, dont allow back travel

--- app/states/MapState.js
+++ app/states/MapState.js
@@ -18,15 +18,18 @@ var MapState = function (game) {
 
     var l1 = new MapLocation(100, 300, 'location 1');
     var l2 = new MapLocation(300, 250, 'location 2');
-    var l3 = new MapLocation(600, 320, 'location 3');
+    var l3 = new MapLocation(500, 180, 'location 3');
+    var l4 = new MapLocation(500, 320, 'location 4');
+    var l5 = new MapLocation(700, 300, 'location 5');
 
     l1.connections = [l2];
-    l2.connections = [l1,l3];
-    l3.connections = [l2];
+    l2.connections = [l3,l4];
+    l3.connections = [l5];
+    l4.connections = [l5];
 
-    this.locations = [l1,l2,l3];
+    this.locations = [l1,l2,l3,l4,l5];
 
-    this.currentLocation = this.locations[0];
+    this.currentLocation = l1;
 };
 
 MapState.prototype = Object.create(State.prototype);
@@ -41,11 +44,10 @@ MapState.prototype.draw = function () {
 
     this.locations.forEach(function (location, ix) {
 
-        if (ix == 0) {
+        location.connections.forEach(function(connection) {
             line.moveTo(location.position.x, location.position.y);
-        } else {
-            line.lineTo(location.position.x, location.position.y);
-        }
+            line.lineTo(connection.position.x, connection.position.y);
+        });
 
         var container = new DisplayObjectContainer();
 
@@ -56,7 +58,7 @@ MapState.prototype.draw = function () {
             gfx.beginFill(0x888888);
         }
 
-        gfx.drawRect(-10, -10, 20, 20);
+        gfx.drawCircle(0, 0, 10);
 
         gfx.interactive = true;
         gfx.buttonMode = true;
@@ -67,7 +69,7 @@ MapState.prototype.draw = function () {
             this.tint = 0xFFFFFF;
         };
         gfx.click = function() {
-            if(location.isConnectedTo(this.currentLocation)) {
+            if(this.currentLocation.isConnectedTo(location)) {
                 this.emit('start-travel', this.currentLocation, location);
             }
         }.bind(this);
